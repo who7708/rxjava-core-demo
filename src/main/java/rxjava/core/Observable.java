d72d98a1e383e1cfa299eeb93a79bda731ec427b
Chris <who7708@163.com>
2022-02-22 23:07:47 +0800

    init

---
+++ src/main/java/rxjava/core/Observable.java
@@ -0,0 +1,9 @@
+package rxjava.core;
+
+/**
+ * @author Chris
+ * @version 1.0.0
+ * @since 2022/02/22
+ */
+public abstract class Observable {
+}
---
+++ src/main/java/rxjava/core/ObservableSource.java
@@ -0,0 +1,12 @@
+package rxjava.core;
+
+/**
+ * 被观察者的顶层接口
+ *
+ * @author Chris
+ * @version 1.0.0
+ * @since 2022/02/22
+ */
+public interface ObservableSource {
+
+}
